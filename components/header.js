8f9d589b916e611581d9a7fe42b28195d2971e9f
ilya smagin <ilya-z1999@ya.ru>
2022-07-24 16:27:04 +0300

    Theme switch button rework

--- components/header.js
+++ components/header.js
@@ -6,7 +6,6 @@ export default function Header() {
       <Link href="/">
         <a className="hover:underline decoration-2">Ла_ла_блог</a>
       </Link>
-      .
     </h2>
   )
 }
--- components/intro.js
+++ components/intro.js
@@ -4,25 +4,38 @@ background: radial-gradient(currentColor 1px, transparent 1px);
 color: white;
 background-size: calc(10 * 1px) calc(10 * 1px);
  */
+function getStorageValue(key, defaultValue) {
+  if (typeof window !== "undefined") {
+    const saved = localStorage.getItem(key);
+    const initial =
+      saved !== null
+        ? JSON.parse(saved)
+        : defaultValue &&
+          window.matchMedia("(prefers-color-scheme: dark)").matches
+        ? defaultValue
+        : !defaultValue;
+    return initial;
+  }
+}
 
-export default function Intro() {
-  const [isDarkTheme, setDarkTheme] = useState(true);
+export const useLocalStorage = (key, defaultValue) => {
+  const [value, setValue] = useState(() => {
+    return getStorageValue(key, defaultValue);
+  });
 
   useEffect(() => {
-    if (typeof window !== "undefined") {
-      if (
-        localStorage.theme === "dark" && isDarkTheme === true ||
-        (!("theme" in localStorage) &&
-          window.matchMedia("(prefers-color-scheme: dark)").matches)
-      ) {
-        document.documentElement.classList.add("dark");
-        setDarkTheme(true);
-      } else {
-        document.documentElement.classList.remove("dark");
-        setDarkTheme(false);
-      }
-    }
-  }, [isDarkTheme]);
+    value
+      ? document.documentElement.classList.add("dark")
+      : document.documentElement.classList.remove("dark");
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+};
+
+export default function Intro() {
+
+  const [darkTheme, setTheme] = useLocalStorage("darkTheme", true);
 
   return (
     <>
@@ -33,10 +46,10 @@ export default function Intro() {
         <h4 className="flex flex-col md:flex-row items-start sm:items-end text-left text-lg mt-12 sm:mt-0">
           Цифровой дизайн блокнот.&nbsp;
           <button
-            onClick={() => setDarkTheme(!isDarkTheme)}
-            className="h-fit underline font-mono hover:decoration-accent-light dark:hover:decoration-accent-dark duration-200 transition-colors"
+            onClick={() => setTheme(!darkTheme)}
+            className="h-fit underline font-mono hover:decoration-blue-600 dark:hover:decoration-accent-dark duration-200 transition-colors"
           >
-            {isDarkTheme ? "Светлая " : "Темная "} тема
+            {darkTheme ? "Светлая " : "Темная "} тема
           </button>
         </h4>
       </section>
